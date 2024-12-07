d2a88471ac6d27732e619ad6439752ec4201e198
saurabh <saurabhlively@yahoo.co.in>
2024-12-07 15:24:08 +0530

    Weather data flask app-Phase1 with port no and data read via pandas

--- FlaskProjects/weather_data_api.py
+++ FlaskProjects/weather_data_api.py
@@ -1,7 +1,9 @@
 import flask
 from flask import Flask,render_template
+import pandas as pd
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
@@ -10,7 +12,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
-    temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df=pd.read_csv(filename,skiprows=20,parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
     return {"station" : station,"date" : date,"temperature" : temperature}
 
 if __name__ == "__main__":
