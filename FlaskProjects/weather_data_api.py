95a4368c6ebdceb2e70848325ee7931f695d1e50
saurabh <saurabhlively@yahoo.co.in>
2024-12-08 14:04:52 +0530

    Added two more api's to query for yearly and all data for one station

--- FlaskProjects/weather_data_api.py
+++ FlaskProjects/weather_data_api.py
@@ -18,5 +18,22 @@ def about(station,date):
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
     return {"station" : station,"date" : date,"temperature" : temperature}
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    df['    DATE']=df['    DATE'].astype(str)
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient='records')
+    return result
+
+
+
 if __name__ == "__main__":
     app.run(debug=True,port=5001)
